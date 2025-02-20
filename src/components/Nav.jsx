0fc2e799a292f40a839e43d5242e467d68c09e6c
Gul DW <gul@dwsolutionline.com>
2025-02-20 14:07:56 +0500

    pushing project code --hanzala kareem--

---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+const Nav = () => {
+    return (
+        <div className='w-full h-30 text-white px-40 flex justify-between items-center z-100 fixed top-0 left-0' >
+            <div>
+                <h1 className='text-2xl font-bold text-center'>Movie Search App</h1>
+            </div>
+            <ul>
+                <li>Developer Portfolio</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Nav
